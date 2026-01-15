a02ea032cc7eb01ecece49994de061ee3dc124b8
meshal <meshal.alhazmi@gmail.com>
2026-01-15 14:38:55 -0600

    assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,7 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-
+ 
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -24,6 +24,7 @@ fi
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
+  echo "${OUTDIR}/linux-stable"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
@@ -33,12 +34,23 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
+     
     # TODO: Add your kernel build steps here
+    echo "make100: Building the Linux kernel"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "make101: Configuring the Linux kernel"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    echo "make102: Building the Linux kernel image"
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    echo "make103: Building the Linux kernel modules "
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo "make104: Building the device tree blobs"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
 fi
 
 echo "Adding the Image in outdir"
-
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -48,33 +60,89 @@ then
 fi
 
 # TODO: Create necessary base directories
-
+mkdir -p ${OUTDIR}/rootfs/bin ${OUTDIR}/rootfs/dev ${OUTDIR}/rootfs/etc ${OUTDIR}/rootfs/home ${OUTDIR}/rootfs/lib ${OUTDIR}/rootfs/lib64 ${OUTDIR}/rootfs/proc ${OUTDIR}/rootfs/sbin ${OUTDIR}/rootfs/sys ${OUTDIR}/rootfs/tmp ${OUTDIR}/rootfs/usr ${OUTDIR}/rootfs/var 
+mkdir -p ${OUTDIR}/rootfs/usr/bin ${OUTDIR}/rootfs/usr/lib ${OUTDIR}/rootfs/usr/sbin ${OUTDIR}/rootfs/var/log
 cd "$OUTDIR"
+echo "Cloning busybox"
+ 
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+     make distclean
+    make defconfig 
 else
     cd busybox
+       
+
 fi
 
 # TODO: Make and install busybox
-
+ echo "make200: Building Busybox"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    echo "make201: Installing Busybox"
+    make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+ 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo "Adding library dependencies to rootfs"
+mkdir -p ${OUTDIR}/rootfs/lib
+mkdir -p ${OUTDIR}/rootfs/lib64
+    echo "Adding ld-linux-aarch64.so.1"
 
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+    echo "Adding libm.so.6"
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib/
+    echo "Adding libresolv.so.2"
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib/
+    echo "Adding libc.so.6"
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib/
+
+
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib64/
+    echo "Adding libm.so.6"
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+    echo "Adding libresolv.so.2"
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+    echo "Adding libc.so.6"
+    cp /home/meshal/Desktop/courses/downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
 # TODO: Make device nodes
-
+mkdir -p "${OUTDIR}/rootfs/dev"
+sudo mknod  -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 622 ${OUTDIR}/rootfs/dev/console c 5 1
 # TODO: Clean and build the writer utility
-
+echo "Building the writer utility"
+cd "$FINDER_APP_DIR"
+ 
+# echo "Removing the old writer utility and compiling as a native application"
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} clean
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 # TODO: Copy the finder related scripts and executables to the /home directory
+cd "$OUTDIR"
+
+mkdir -p ${OUTDIR}/rootfs/home
+
+cp -r ${FINDER_APP_DIR}/ ${OUTDIR}/rootfs/home/ 
+rm -rf ${OUTDIR}/rootfs/home/finder-app/conf
+mkdir -p ${OUTDIR}/rootfs/home/finder-app/conf
+mkdir -p ${OUTDIR}/rootfs/home/conf
+
+cp -R ${FINDER_APP_DIR}/conf/* ${OUTDIR}/rootfs/home/finder-app/conf/
+ cp -R ${FINDER_APP_DIR}/conf/* ${OUTDIR}/rootfs/home/conf/
+
+
 # on the target rootfs
 
 # TODO: Chown the root directory
-
+sudo chown -R root:root ${OUTDIR}/rootfs
 # TODO: Create initramfs.cpio.gz
+echo "Creating initramfs.cpio.gz"
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov  --owner root:root   > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+echo "initramfs.cpio.gz created at ${OUTDIR}/initramfs.cpio.gz"
